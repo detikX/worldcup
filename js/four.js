915f3e23218d54abb9b5860314c2f6a96d0e73df
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-11-03 20:31:04 +0700

    update

--- js/four.js
+++ js/four.js
@@ -30,8 +30,8 @@ $('.slider').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: false
         }
