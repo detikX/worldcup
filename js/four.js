2357b0661819fa5316fc209af8749863348583fd
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-11-07 13:30:37 +0700

    update

--- js/four.js
+++ js/four.js
@@ -1,3 +1,102 @@
+gsap.registerPlugin(ScrollTrigger);
+
+        var targetsEndTrigger = document.querySelectorAll(".endtrigger");
+
+        var targetsHeight = document.querySelectorAll(".height");
+
+        var targetsBreak = document.querySelectorAll(".break");
+
+        targetsEndTrigger.forEach((target, index) => {
+          const pinSpacing = index === targetsEndTrigger.length - 1 ? "true" : false;
+          const endTrigger = `#card${index + 1}`
+          const opacityFirst = index === 0 ? 1 : 0;
+          const opacitySecond = index === targetsEndTrigger.length - 1 ? 1 : 0;
+          const tl = gsap
+            .timeline({
+              defaults: { duration: 1 },
+              scrollTrigger: {
+                trigger: target,
+                pin: true,
+                scrub: true,
+                start: "center center",
+                // end,
+                endTrigger,
+                markers: false,
+                toggleActions: "restart none reverse reset",
+                pinSpacing,
+              },
+            })
+            .from(target, { opacity: opacityFirst, duration: 0.5 }, 0)
+            .to(target, { opacity: opacitySecond, duration: 0.5 }, 0.8);
+        });
+
+        targetsHeight.forEach((target, index) => {
+          const pinSpacing = index === targetsHeight.length - 1 ? "true" : false;
+          const end =
+            index === targetsHeight.length - 1
+              ? `+=${target.offsetHeight}px`
+              : `+=${targetsHeight[index + 1].offsetHeight}px`;
+          const opacityFirst = index === 0 ? 1 : 0;
+          const opacitySecond = index === targetsHeight.length - 1 ? 1 : 0;
+          const tl = gsap
+            .timeline({
+              defaults: { duration: 1 },
+              scrollTrigger: {
+                trigger: target,
+                pin: true,
+                scrub: true,
+                start: "center center",
+                end,
+                markers: false,
+                toggleActions: "restart none reverse reset",
+                pinSpacing,
+              },
+            })
+            .from(target, { opacity: opacityFirst, duration: 0.5 }, 0)
+            .to(target, { opacity: opacitySecond, duration: 0.5 }, 0.8);
+        });
+
+        targetsBreak.forEach((target, index) => {
+          const pinSpacing = index === targetsBreak.length - 1 ? "true" : false;
+          const end =
+            index === targetsBreak.length - 1
+              ? `+=${target.offsetHeight}px`
+              : `+=${targetsBreak[index + 1].offsetHeight}px`;
+          const opacityFirst = index === 0 ? 1 : 0;
+          const opacitySecond = index === targetsBreak.length - 1 ? 1 : 0;
+          const tl = gsap
+            .timeline({
+              defaults: { duration: 1 },
+              scrollTrigger: {
+                trigger: target,
+                pin: true,
+                scrub: true,
+                start: "center center",
+                end,
+                markers: false,
+                toggleActions: "restart none reverse reset",
+                pinSpacing,
+              },
+            })
+            .from(target, { opacity: opacityFirst, duration: 0.5 }, 0)
+            .to(target, { opacity: opacitySecond, duration: 0.5 }, 0.8);
+        });
+
+        //fancy box
+        $('[data-fancybox="gallery"]').fancybox({
+          buttons: [
+            "slideShow",
+            "thumbs",
+            "zoom",
+            "fullScreen",
+            "share",
+            "close"
+          ],
+          loop: false,
+          protect: true
+        });
+
+
 $(".scroll-totop").hide();
 		// fade in #back-top
 		$(function () {
@@ -20,12 +119,13 @@ $(".scroll-totop").hide();
       duration: 1200,
     })
 
-$('.slider').slick({
+$('.sliderx').slick({
     dots: false,
     infinite: false,
     speed: 300,
     slidesToShow: 3.2,
     slidesToScroll: 2,
+    autoplay:false,
     responsive: [
       {
         breakpoint: 1024,
